2ac532d9e09dd6b037fa67a9d539ef54be576541
Huaji <726471847@qq.com>
2016-10-17 22:47:48 +0800

    buttonknife

---
+++ app/src/main/java/ucai/cn/fulicter/activity/MainActivity.java
@@ -0,0 +1,67 @@
+package ucai.cn.fulicter.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import ucai.cn.fulicter.R;
+import ucai.cn.fulicter.utils.L;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int index;
+    RadioButton [] rbs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        L.i("MainActiviry onCreate");
+        initView();
+    }
+
+    private void initView() {
+        rbs = new RadioButton[5];
+        rbs[0]=mlayoutNewGood;
+        rbs[1]=mlayoutBoutique;
+        rbs[2]=mlayoutCategory;
+        rbs[3]=mlayoutCart;
+        rbs[4]=mlayoutPersonalCenter;
+    }
+
+
+    public void onCheckedChange(View v) {
+        switch (v.getId()) {
+            case R.id.layout_new_good:
+                index=0;
+                break;
+            case R.id.layout_boutique:
+                index=1;
+                break;
+            case R.id.layout_category:
+                index=2;
+                break;
+            case R.id.layout_cart:
+                index=3;
+                break;
+            case R.id.layout_personal_center:
+                index=4;
+                break;
+        }
+        setRadioButtonSataus();
+    }
+
+    private void setRadioButtonSataus() {
+        for (int i=0;i<rbs.length;i++) {
+            if (i == index) {
+                rbs[i].setChecked(true);
+            }else{
+                rbs[i].setChecked(false);
+            }
+        }
+    }
+}
