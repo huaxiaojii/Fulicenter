1b056e6e4101ea43dc0e46dbf6c810ad8d3e414d
Huaji <726471847@qq.com>
2016-10-19 02:27:17 +0800

    下载数据完成之后处理刷新后的页面

--- app/src/main/java/ucai/cn/fulicter/fragment/NewGoodsFragment.java
+++ app/src/main/java/ucai/cn/fulicter/fragment/NewGoodsFragment.java
@@ -1,9 +1,9 @@
 package ucai.cn.fulicter.fragment;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,15 +16,14 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import cn.ucai.xm_fulicenter.Activity.MainActivity;
-import cn.ucai.xm_fulicenter.I;
-import cn.ucai.xm_fulicenter.R;
-import cn.ucai.xm_fulicenter.adapter.GoodsAdapter;
-import cn.ucai.xm_fulicenter.bean.NewGoodsBean;
-import cn.ucai.xm_fulicenter.net.NetDao;
-import cn.ucai.xm_fulicenter.net.OkHttpUtils;
-import cn.ucai.xm_fulicenter.utils.ConvertUtils;
-import cn.ucai.xm_fulicenter.utils.L;
+import ucai.cn.fulicter.activity.MainActivity;
+import ucai.cn.fulicter.adapter.GoodsAdapter;
+import ucai.cn.fulicter.bean.NewGoodsBean;
+import ucai.cn.fulicter.net.NetDao;
+import ucai.cn.fulicter.net.OkHttpUtils;
+import ucai.cn.fulicter.utils.Commonutils;
+import ucai.cn.fulicter.utils.L;
+
 
 /**
  * Created by yanglei on 2016/10/17.
@@ -42,6 +41,7 @@ public class NewGoodsFragment extends Fragment {
     GoodsAdapter mAdapter;
     ArrayList<NewGoodsBean> mlist;
     int pageId=1;
+    boolean isMore;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -53,20 +53,39 @@ public class NewGoodsFragment extends Fragment {
         initView();
         initData();
         return layout;
+
+    }
+        public boolean isMore(){
+
+            return isMore;
+        }
+    public void setMore(boolean more){
+
+        isMore=more;
     }
 
     private void initData() {
         NetDao.downloadNewGoods(mcontext, pageId, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
             @Override
             public void onSuccess(NewGoodsBean[] result) {
+                msrl.setRefreshing(false);
+                mtvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(true);
+                L.e("result="+result);
                 if (result!= null && result.length > 0) {
                     ArrayList<NewGoodsBean> List = ConvertUtils.array2List(result);
                     mAdapter.initData(List);
+                    if (mlist.size())<I.PAGE_SIZE_DEFAULT{
+                        mAdapter.setMore(false);
+                    }
+                }else {
+                    mAdapter.setMore(false);
                 }
             }
 
             @Override
             public void onError(String error) {
+
                 L.e("error"+error);
             }
         });
