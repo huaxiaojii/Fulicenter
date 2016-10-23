d0173b13b960ee686e3d67a6e5c0efd8751a8577
Huaji <726471847@qq.com>
2016-10-23 20:51:29 +0800

    实现用户注册

--- app/src/main/java/ucai/cn/fulicter/activity/ResgisterActivity.java
+++ app/src/main/java/ucai/cn/fulicter/activity/ResgisterActivity.java
@@ -1,18 +1,29 @@
 package ucai.cn.fulicter.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ucai.cn.fulicter.bean.Result;
+import ucai.cn.fulicter.net.NetDao;
+import ucai.cn.fulicter.net.OkHttpUtils;
+import ucai.cn.fulicter.utils.CommonUtils;
+import ucai.cn.fulicter.utils.L;
+import ucai.cn.fulicter.utils.MFGT;
 import ucai.cn.fulicter.view.DisplayUtils;
 
 /**
  * Created by User on 2016/10/23.
  */
 
-public class RegisterActivity extends BaseActivity {
+public class ResgisterActivity extends BaseActivity{
+    private static final String TAG = ResgisterActivity.class.getSimpleName();
 
     @BindView(R.id.username)
     EditText mUsername;
@@ -25,16 +36,23 @@ public class RegisterActivity extends BaseActivity {
     @BindView(R.id.btn_register)
     Button mBtnRegister;
 
+
+    String username;
+    String nickname;
+    String password;
+    RecyclerView mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+        mContext = this;
         super.onCreate(savedInstanceState);
     }
 
     @Override
     protected void initView() {
-        DisplayUtils.initBackWithTitle(this,"账户注册");
+        DisplayUtils.initBackWithTitle(this, "账户注册");
     }
 
     @Override
@@ -46,4 +64,69 @@ public class RegisterActivity extends BaseActivity {
     protected void setListener() {
 
     }
+
+    @OnClick(R.id.btn_register)
+    public void checkedInput() {
+        username = mUsername.getText().toString().trim();
+        nickname = mNick.getText().toString().trim();
+        password = mPassword.getText().toString().trim();
+        String confirmPwd = mConfirmPassword.getText().toString().trim();
+        if(TextUtils.isEmpty(username)){
+            CommonUtils.showShortToast(R.string.user_name_connot_be_empty);
+            mUsername.requestFocus();
+            return;
+        }else if(!username.matches("[a-zA-Z]\\w{5,15}")){
+            CommonUtils.showShortToast(R.string.illegal_user_name);
+            mUsername.requestFocus();
+            return;
+        }else if(TextUtils.isEmpty(nickname)){
+            CommonUtils.showShortToast(R.string.nick_name_connot_be_empty);
+            mNick.requestFocus();
+            return;
+        }else if(TextUtils.isEmpty(password)){
+            CommonUtils.showShortToast(R.string.password_connot_be_empty);
+            mPassword.requestFocus();
+            return;
+        }else if(TextUtils.isEmpty(confirmPwd)){
+            CommonUtils.showShortToast(R.string.confirm_password_connot_be_empty);
+            mConfirmPassword.requestFocus();
+            return;
+        }else if(!password.equals(confirmPwd)){
+            CommonUtils.showShortToast(R.string.two_input_password);
+            mConfirmPassword.requestFocus();
+            return;
+        }
+        register();
+    }
+
+    private void register() {
+        final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.registering));
+        pd.show();
+        NetDao.register(mContext, username, nickname, password, new OkHttpUtils.OnCompleteListener<Result>() {
+            @Override
+            public void onSuccess(Result result) {
+                pd.dismiss();
+                if(result==null){
+                    CommonUtils.showShortToast(R.string.register_fail);
+                }else{
+                    if(result.isRetMsg()){
+                        CommonUtils.showLongToast(R.string.register_success);
+                        MFGT.finish(mContext);
+                    }else{
+                        CommonUtils.showLongToast(R.string.register_fail_exists);
+                        mUsername.requestFocus();
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showShortToast(error);
+                L.e(TAG,"register error="+error);
+            }
+        });
+    }
+}
 }
--- app/src/main/java/ucai/cn/fulicter/net/NetDao.java
+++ app/src/main/java/ucai/cn/fulicter/net/NetDao.java
@@ -9,10 +9,13 @@ import ucai.cn.fulicter.bean.CategoryChildBean;
 import ucai.cn.fulicter.bean.CategoryGroupBean;
 import ucai.cn.fulicter.bean.GoodsDetailsBean;
 import ucai.cn.fulicter.bean.NewGoodsBean;
+import ucai.cn.fulicter.bean.Result;
+import ucai.cn.fulicter.utils.MD5;
 
 /**
  * Created by Winston on 2016/10/17.
  */
+
 public class NetDao {
     public static void downloadNewGoods(Context context,int catId, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener){
         OkHttpUtils utils = new OkHttpUtils(context);
@@ -40,19 +43,37 @@ public class NetDao {
     }
 
     public static void downloadCategoryGroup(Context context, OkHttpUtils.OnCompleteListener<CategoryGroupBean[]> listener){
-        OkHttpUtils utils =new OkHttpUtils(context);
+        OkHttpUtils utils = new OkHttpUtils(context);
         utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_GROUP)
                 .targetClass(CategoryGroupBean[].class)
                 .execute(listener);
     }
+
     public static void downloadCategoryChild(Context context,int parentId, OkHttpUtils.OnCompleteListener<CategoryChildBean[]> listener){
-        OkHttpUtils utils =new OkHttpUtils(context);
+        OkHttpUtils utils = new OkHttpUtils(context);
         utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_CHILDREN)
                 .addParam(I.CategoryChild.PARENT_ID,String.valueOf(parentId))
                 .targetClass(CategoryChildBean[].class)
                 .execute(listener);
     }
 
-
+    public static void downloadCategoryGoods(Context context,int catId, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener){
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_GOODS_DETAILS)
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID,String.valueOf(catId))
+                .addParam(I.PAGE_ID,String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_SIZE_DEFAULT))
+                .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+    }
+    public static void register(Context context, String username, String nickname, String password, OkHttpUtils.OnCompleteListener<Result> listener){
+        OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,nickname)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(Result.class)
+                .post()
+                .execute(listener);
+    }
 }
-
