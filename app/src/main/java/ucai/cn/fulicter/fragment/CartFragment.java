603089f828a61872f8b7196b21b9baa575144ece
Huaji <726471847@qq.com>
2016-10-27 23:09:24 +0800

    车车内商品价格显示

--- app/src/main/java/ucai/cn/fulicter/fragment/CartFragment.java
+++ app/src/main/java/ucai/cn/fulicter/fragment/CartFragment.java
@@ -8,12 +8,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import ucai.cn.fulicter.FuLiCenterApplication;
 import ucai.cn.fulicter.activity.MainActivity;
@@ -35,7 +37,7 @@ public class CartFragment extends BaseFragment {
     private static final String TAG = CartFragment.class.getSimpleName();
     @BindView(R.id.tv_refresh)
     TextView mTvRefresh;
-    @BindView(R.id.rvNewGoods)
+    @BindView(R.id.rv)
     RecyclerView mRv;
     @BindView(R.id.srl)
     SwipeRefreshLayout mSrl;
@@ -43,16 +45,24 @@ public class CartFragment extends BaseFragment {
     MainActivity mContext;
     CartAdapter mAdapter;
     ArrayList<CartBean> mList;
+    @BindView(R.id.tv_cart_sum_price)
+    TextView mTvCartSumPrice;
+    @BindView(R.id.tv_cart_save_price)
+    TextView mTvCartSavePrice;
+    @BindView(R.id.layout_cart)
+    RelativeLayout mLayoutCart;
+    @BindView(R.id.tv_nothing)
+    TextView mTvNothing;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_newgoods, container, false);
+        View layout = inflater.inflate(R.layout.fragment_cart, container, false);
         ButterKnife.bind(this, layout);
         mContext = (MainActivity) getContext();
         mList = new ArrayList<>();
-        mAdapter = new CartAdapter(mContext,mList);
-        super.onCreateView(inflater,container,savedInstanceState);
+        mAdapter = new CartAdapter(mContext, mList);
+        super.onCreateView(inflater, container, savedInstanceState);
         return layout;
     }
 
@@ -73,54 +83,43 @@ public class CartFragment extends BaseFragment {
     }
 
     @Override
-    protected  void initData() {
+    protected void initData() {
         downloadCart();
     }
 
     private void downloadCart() {
         User user = FuLiCenterApplication.getUser();
-        if(user!=null){
-//            NetDao.downloadCart(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<CartBean[]>() {
-//                @Override
-//                public void onSuccess(CartBean[] result) {
-//                    L.e(TAG,"result="+result);
-//                    mSrl.setRefreshing(false);
-//                    mTvRefresh.setVisibility(View.GONE);
-//                    if(result!=null && result.length>0){
-//                        ArrayList<CartBean> list = ConvertUtils.array2List(result);
-//                        mAdapter.initData(list);
-//                    }
-//                }
-
+        if (user != null) {
             NetDao.downloadCart(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<String>() {
-
-
-
-
                 @Override
-                public void onSuccess(String result) {
+                public void onSuccess(String s) {
                     ArrayList<CartBean> list = ResultUtils.getCartFromJson(s);
-                    L.e(TAG,"result="+list);
+                    L.e(TAG, "result=" + list);
                     mSrl.setRefreshing(false);
                     mTvRefresh.setVisibility(View.GONE);
-                    if(list!=null && list.size()>0){
-                        L.e(TAG,"list[0]="+list.get(0));
+                    if (list != null && list.size() > 0) {
+                        L.e(TAG, "list[0]=" + list.get(0));
+                        mAdapter.initData(list);
+                        setCartLayout(true);
+                    }else{
+                        setCartLayout(false);
                     }
                 }
 
                 @Override
                 public void onError(String error) {
+                    setCartLayout(false);
                     mSrl.setRefreshing(false);
                     mTvRefresh.setVisibility(View.GONE);
                     CommonUtils.showShortToast(error);
-                    L.e("error:"+error);
+                    L.e("error:" + error);
                 }
             });
         }
     }
 
     @Override
-    protected  void initView() {
+    protected void initView() {
         mSrl.setColorSchemeColors(
                 getResources().getColor(R.color.google_blue),
                 getResources().getColor(R.color.google_green),
@@ -132,5 +131,40 @@ public class CartFragment extends BaseFragment {
         mRv.setHasFixedSize(true);
         mRv.setAdapter(mAdapter);
         mRv.addItemDecoration(new SpaceItemDecoration(12));
+        setCartLayout(false);
+    }
+
+    private void setCartLayout(boolean hasCart) {
+        mLayoutCart.setVisibility(hasCart?View.VISIBLE:View.GONE);
+        mTvNothing.setVisibility(hasCart?View.GONE:View.VISIBLE);
+        mRv.setVisibility(hasCart?View.VISIBLE:View.GONE);
+        sumPrice();
+    }
+
+    @OnClick(R.id.tv_cart_buy)
+    public void onClick() {
+    }
+
+    private void sumPrice(){
+        int sumPrice = 0;
+        int rankPrice = 0;
+        if(mList!=null && mList.size()>0){
+            for (CartBean c:mList){
+                if(c.isChecked()){
+                    sumPrice += getPrice(c.getGoods().getCurrencyPrice())*c.getCount();
+                    rankPrice += getPrice(c.getGoods().getRankPrice())*c.getCount();
+                }
+            }
+            mTvCartSumPrice.setText("合计:￥"+Double.valueOf(sumPrice));
+            mTvCartSavePrice.setText("节省:￥"+Double.valueOf(sumPrice-rankPrice));
+
+        }else{
+            mTvCartSumPrice.setText("合计:￥0");
+            mTvCartSavePrice.setText("节省:￥0");
+        }
+    }
+    private int getPrice(String price){
+        price = price.substring(price.indexOf("￥")+1);
+        return Integer.valueOf(price);
     }
 }
