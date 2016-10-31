830067c4f5e62cca43f55163eb0fd08fd2da48dd
Huaji <726471847@qq.com>
2016-10-31 08:36:18 +0800

    合入壹收款支付功能

--- app/src/main/java/ucai/cn/fulicter/activity/OrderActivity.java
+++ app/src/main/java/ucai/cn/fulicter/activity/OrderActivity.java
@@ -1,20 +1,29 @@
 package ucai.cn.fulicter.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import ucai.cn.fulicter.FuLiCenterApplication;
 import ucai.cn.fulicter.I;
+import ucai.cn.fulicter.bean.CartBean;
+import ucai.cn.fulicter.bean.User;
+import ucai.cn.fulicter.net.NetDao;
+import ucai.cn.fulicter.net.OkHttpUtils;
 import ucai.cn.fulicter.utils.L;
+import ucai.cn.fulicter.utils.ResultUtils;
+import ucai.cn.fulicter.view.DisplayUtils;
 
-/**
- * Created by User on 2016/10/30.
- */
+
 
 public class OrderActivity extends BaseActivity {
     private static final String TAG = OrderActivity.class.getSimpleName();
@@ -30,22 +39,79 @@ public class OrderActivity extends BaseActivity {
     @BindView(R.id.tv_order_price)
     TextView mTvOrderPrice;
 
+    OrderActivity mContext;
+    User user = null;
+    String cartIds = "";
+    ArrayList<CartBean> mList = null;
+    String[] ids = new String[]{};
+    int rankPrice = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_order);
         ButterKnife.bind(this);
+        mContext = this;
+        mList = new ArrayList<>();
         super.onCreate(savedInstanceState);
     }
 
     @Override
     protected void initView() {
-
+        DisplayUtils.initBackWithTitle(mContext,getString(R.string.confirm_order));
     }
 
     @Override
     protected void initData() {
-        String cartIds = getIntent().getStringExtra(I.Cart.ID);
+        cartIds = getIntent().getStringExtra(I.Cart.ID);
+        user = FuLiCenterApplication.getUser();
         L.e(TAG,"cartIds="+cartIds);
+        if(cartIds==null || cartIds.equals("")
+                || user==null){
+            finish();
+        }
+        ids = cartIds.split(",");
+        geOrderList();
+    }
+
+    private void geOrderList() {
+        NetDao.downloadCart(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                ArrayList<CartBean> list = ResultUtils.getCartFromJson(s);
+                if(list==null || list.size()==0){
+                    finish();
+                }else{
+                    mList.addAll(list);
+                    sumPrice();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+    }
+
+    private void sumPrice() {
+        rankPrice = 0;
+        if(mList!=null && mList.size()>0){
+            for (CartBean c:mList){
+                L.e(TAG,"c.id="+c.getId());
+                for (String id:ids) {
+                    L.e(TAG,"order.id="+id);
+                    if (id.equals(String.valueOf(c.getId()))) {
+                        rankPrice += getPrice(c.getGoods().getRankPrice()) * c.getCount();
+                    }
+                }
+            }
+        }
+        mTvOrderPrice.setText("合计:￥"+Double.valueOf(rankPrice));
+    }
+
+    private int getPrice(String price){
+        price = price.substring(price.indexOf("￥")+1);
+        return Integer.valueOf(price);
     }
 
     @Override
@@ -54,6 +120,39 @@ public class OrderActivity extends BaseActivity {
     }
 
     @OnClick(R.id.tv_order_buy)
-    public void onClick() {
+    public void checkOrder() {
+        String receiveName=mEdOrderName.getText().toString();
+        if(TextUtils.isEmpty(receiveName)){
+            mEdOrderName.setError("收货人姓名不能为空");
+            mEdOrderName.requestFocus();
+            return;
+        }
+        String mobile=mEdOrderPhone.getText().toString();
+        if(TextUtils.isEmpty(mobile)){
+            mEdOrderPhone.setError("手机号码不能为空");
+            mEdOrderPhone.requestFocus();
+            return;
+        }
+        if(!mobile.matches("[\\d]{11}")){
+            mEdOrderPhone.setError("手机号码格式错误");
+            mEdOrderPhone.requestFocus();
+            return;
+        }
+        String area=mSpinOrderProvince.getSelectedItem().toString();
+        if(TextUtils.isEmpty(area)){
+            Toast.makeText(OrderActivity.this,"收货地区不能为空",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String address=mEdOrderStreet.getText().toString();
+        if(TextUtils.isEmpty(address)){
+            mEdOrderStreet.setError("街道地址不能为空");
+            mEdOrderStreet.requestFocus();
+            return;
+        }
+        gotoStatements();
+    }
+
+    private void gotoStatements() {
+        L.e(TAG,"rankPrice="+rankPrice);
     }
 }
